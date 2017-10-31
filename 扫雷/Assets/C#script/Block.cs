ed9bec2c3cc4d6fe7e28dfe37a4378bd21128cd0
parasiter <937247098@qq.com>
2017-10-31 21:33:50 +0800

    刚开始
    初始化背景

---
+++ 扫雷/Assets/C#script/Block.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Block : MonoBehaviour {
+	[SerializeField] private GameObject blockBack;
+	// Use this for initialization
+	void Start () {
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+	public void OnMouseDown(){
+		if (blockBack.activeSelf)
+			blockBack.SetActive (false);
+	}
+	public void SetImage(Sprite image)
+	{
+		GetComponent<SpriteRenderer> ().sprite = image;
+	}
+}
---
+++ 扫雷/Assets/C#script/SceneController.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SceneController : MonoBehaviour {
+	public const int gridRows = 10;
+	public const int gridCols = 10;
+	public const float offset = 1.0f;
+
+	[SerializeField] private Block originalBlock;
+	[SerializeField] private Line originalLine;
+	[SerializeField] private Sprite[] images;
+	// Use this for initialization
+	void Start () {
+		Vector3 blockStartPos = originalBlock.transform.position;
+		Vector3 lineStartPos = originalLine.transform.position;
+
+		for (int i = 0; i < gridRows; ++i) {
+			for (int j = 0; j < gridCols; ++j) {
+				Block newBlock;
+				if (i == 0 && j == 0)
+					newBlock = originalBlock;
+				else {
+					newBlock = Instantiate (originalBlock) as Block;
+					newBlock.SetImage (images [0]);
+					newBlock.transform.position = new Vector3 (blockStartPos.x + offset * i, blockStartPos.y - offset * j, blockStartPos.z);
+				}
+			}
+		}
+
+		Line vertLine = Instantiate (originalLine)as Line;
+		vertLine.transform.Rotate (new Vector3 (0.0f, 0.0f, 90.0f));
+		for (int i = 0; i <= 5; ++i) {			
+			Line newLine;
+			newLine = Instantiate (originalLine) as Line;
+			newLine.transform.position = new Vector3 (lineStartPos.x, lineStartPos.y + i, lineStartPos.z);
+			newLine = Instantiate (originalLine) as Line;
+			newLine.transform.position = new Vector3 (lineStartPos.x, lineStartPos.y - i, lineStartPos.z);
+
+			newLine = Instantiate (vertLine) as Line;
+			newLine.transform.position = new Vector3 (lineStartPos.x - i, lineStartPos.y, lineStartPos.z);
+			newLine = Instantiate (vertLine) as Line;
+			newLine.transform.position = new Vector3 (lineStartPos.x + i, lineStartPos.y, lineStartPos.z);
+		}
+	}
+		
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
